4600f51225421701961888055ebd4d77a728305f
jammin804 <jammin804@gmail.com>
2014-10-23 16:22:55 -0400

    Add Class ParseContants. Now User relations appear on parse dashboard

---
+++ app/src/main/java/com/mikejharris/rabbit/ParseConstants.java
@@ -0,0 +1,12 @@
+package com.mikejharris.rabbit;
+
+/**
+ * Created by Mike on 10/23/2014.
+ */
+public final class ParseConstants {
+    //Class Names
+
+    //Field Names
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+}
